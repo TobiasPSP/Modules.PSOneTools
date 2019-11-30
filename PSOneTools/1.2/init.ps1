ad6ff45c2a85bbc1a0b5919b3c65adb126e35db0
tobiaspsp <tobias@powershell.one>
2019-11-30 18:11:40 +0100

    Version 1.1
    published version 1.1

--- PSOneTools/1.2/init.ps1
+++
@@ -1,7 +0,0 @@
-﻿
-# use this file to define global variables on module scope
-# or perform other initialization procedures.
-# this file will not be touched when new functions are exported to
-# this module.
-
-
